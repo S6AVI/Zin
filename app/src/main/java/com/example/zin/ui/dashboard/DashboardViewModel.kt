0f7b90bb7bb7c01504c26f57ea9bbf07bf0fce6d
2.0 <51497609+S6AVI@users.noreply.github.com>
2022-08-18 01:50:59 +0300

    Update DashboardViewModel.kt

--- app/src/main/java/com/example/zin/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/zin/ui/dashboard/DashboardViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class DashboardViewModel(
+        private val dataSource: BahrDatabaseDao,
+        private val application: Application): AndroidViewModel(application) {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
